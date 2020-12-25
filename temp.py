09b1aafebc5a8f060f2ce333c8a1e407a3be85fc
cyber-holmes <67431536+cyber-holmes@users.noreply.github.com>
2020-12-25 17:15:54 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,11 @@
+#Goal:Convert the given temperature from Celsius to Farenheit.
+
+#Step1:Take the user-input on temperature the user wish to convert. 
+cel= input("Enter your temperature in Celsius ")
+
+#Step2:Calculate the conversion using formula (celsius*1.8)+32
+far = (cel*1.8)+32
+
+#Step3:Print the output.
+print ("It is {} Farenheit".format(far))
+
